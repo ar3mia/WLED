34b370b4f2a8fdde9511f526e20fc5c084ce20f1
ar3asault <ar3asault@gmail.com>
2022-05-04 10:20:05 -0400

    Revert "Updated May 2022"
    This reverts commit c140f4e2354d0a349d7e5bff85f59f36284e9d45.

--- wled00/FX.cpp
+++ wled00/FX.cpp
@@ -4170,118 +4170,60 @@ class AuroraWave {
     };
 };
 
-const boolean segmentArray[11][7] = {{0, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 0, 1, 1, 1}, {1, 0, 0, 0, 1, 0, 0},
-                                     {1, 1, 0, 1, 0, 1, 1}, {1, 1, 0, 1, 1, 1, 0}, {1, 0, 1, 1, 1, 0, 0},
-                                     {0, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 1, 1, 1, 1}, {1, 1, 0, 0, 1, 0, 0},
-                                     {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 0}
-};
-#define USE_COLON_SEPARATOR
-
 uint16_t WS2812FX::mode_aurora(void) {
-     uint8_t timeSecond = second(localTime);
-  uint8_t timeMinute = minute(localTime);
+  //aux1 = Wavecount
+  //aux2 = Intensity in last loop
 
-  uint8_t ledsPerSegment = 9;       //9 LEDs per Segmentfraction (7 Segment with each 9 LEDs = 63 LEDs)
-  uint8_t numberOfSegments = 4;     //2x Hours, 2x Minutes
+  AuroraWave* waves;
 
-  uint8_t timeHour = 0;             //Bypass compiler error
-  uint8_t limit = 0;                //Bypass compiler error, sets it according to 12/24h format
-  
-  if(!useAMPM) {
-    limit = 0;
-    timeHour = hour(localTime);
+  if(SEGENV.aux0 != SEGMENT.intensity || SEGENV.call == 0) {
+    //Intensity slider changed or first call
+    SEGENV.aux1 = map(SEGMENT.intensity, 0, 255, 2, W_MAX_COUNT);
+    SEGENV.aux0 = SEGMENT.intensity;
+
+    if(!SEGENV.allocateData(sizeof(AuroraWave) * SEGENV.aux1)) { // 26 on 32 segment ESP32, 9 on 16 segment ESP8266
+      return mode_static(); //allocation failed
+    }
+
+    waves = reinterpret_cast<AuroraWave*>(SEGENV.data);
+
+    for(int i = 0; i < SEGENV.aux1; i++) {
+      waves[i].init(SEGLEN, col_to_crgb(color_from_palette(random8(), false, false, random(0, 3))));
+    }
+  } else {
+    waves = reinterpret_cast<AuroraWave*>(SEGENV.data);
   }
-  else {
-    limit = 1;
-    timeHour = hour(localTime) % 12;
-    if(timeHour == 0) timeHour = 12;
+
+  for(int i = 0; i < SEGENV.aux1; i++) {
+    //Update values of wave
+    waves[i].update(SEGLEN, SEGMENT.speed);
+
+    if(!(waves[i].stillAlive())) {
+      //If a wave dies, reinitialize it starts over.
+      waves[i].init(SEGLEN, col_to_crgb(color_from_palette(random8(), false, false, random(0, 3))));
+    }
   }
 
-  #ifdef USE_COLON_SEPARATOR
-    uint8_t ledsPerDot = 1;         //1 LED per seconds dot, so 2 in total
-    #define dotColor 0x999999
-  #endif
+  uint8_t backlight = 1; //dimmer backlight if less active colors
+  if (SEGCOLOR(0)) backlight++;
+  if (SEGCOLOR(1)) backlight++;
+  if (SEGCOLOR(2)) backlight++;
+  //Loop through LEDs to determine color
+  for(int i = 0; i < SEGLEN; i++) {    
+    CRGB mixedRgb = CRGB(backlight, backlight, backlight);
 
-
-  uint8_t digits[4];
-  digits[0] = timeHour / 10;
-  digits[1] = timeHour % 10;
-  digits[2] = timeMinute / 10;
-  digits[3] = timeMinute % 10;
-
-  /*digits[3] = timeHour / 10;
-  digits[2] = timeHour % 10;
-  digits[1] = timeMinute / 10;
-  digits[0] = timeMinute % 10;*/
-  //Reversed order for my own setup
-
-  uint8_t index = 0;
-  uint8_t usedIndex = 0;        //Used for better Color Palette usage
-  uint8_t counter = (now * ((SEGMENT.speed >> 2) + 2)) & 0xFFFF;
-  
-  for (int i = numberOfSegments - 1; i >= limit; i--) {      //Cycle through digits
-    for (uint8_t j = 0; j < 7; j++) {                   //Cycle through each part of a digit
-      for (uint8_t k = 0; k < ledsPerSegment; k++) {    //Cycle through each led per part of a digit
-        if(segmentArray[digits[i]+1][j]) {          //If LEDs should light up:
-          usedIndex++;
-          if(!useAMPM) {
-            if(i == 3 || i == 2) {      //Minutes
-              setPixelColor(index, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 0));
-            }
-            else if(i == 1 || i == 0) { //Hours
-              setPixelColor(index, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 1));
-            }
-          }
-          else {
-            if(i == 3 || i == 2) {      //Minutes
-              setPixelColor(index, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 0));
-            }
-            else if(i == 1) {           //Hours
-              setPixelColor(index, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 1));
-            }
-          }
-        }
-        else {      //LEDs go black
-          setPixelColor(index, 0);
-        }
-        index++;
+    //For each LED we must check each wave if it is "active" at this position.
+    //If there are multiple waves active on a LED we multiply their values.
+    for(int  j = 0; j < SEGENV.aux1; j++) {
+      CRGB rgb = waves[j].getColorForLED(i);
+      
+      if(rgb != CRGB(0)) {       
+        mixedRgb += rgb;
       }
     }
-    #ifdef USE_COLON_SEPARATOR
-      if(numberOfSegments - 2 == i) {
-        if(timeSecond % 2 == 0) {                                                           //Light up on even seconds
-          for(uint8_t l = 0; l < ledsPerDot*2;l++) {
-            setPixelColor(index, dotColor);
-            index++;
-          }
-        }
-        else {
-          for(uint8_t l = 0; l < ledsPerDot*2;l++) {
-            setPixelColor(index, 0);
-            index++;
-          }
-        }
-      }
-    #endif
-  }
-  if(useAMPM) {
-    if(digits[0] > 0) {
-      for(uint8_t i = 0;i < 2*ledsPerSegment;i++) {
-        setPixelColor(index+i, color_from_palette(usedIndex*map(SEGMENT.intensity, 0, 255, 0, 25)+(now / map(SEGMENT.speed, 0, 255, 400, 5)), false, true, 1));
-      }
-    }
-    else {
-      for(uint8_t i = 0;i < 2*ledsPerSegment;i++) {
-        setPixelColor(index+i, 0);
-      }
-    }
-    index += 2*ledsPerSegment;
-  }
 
+    setPixelColor(i, mixedRgb[0], mixedRgb[1], mixedRgb[2]);
+  }
   
-  for(uint16_t i = index; i < SEGLEN;i++) {
-    setPixelColor(i, SEGCOLOR(2));
-  }
-
   return FRAMETIME;
 }
